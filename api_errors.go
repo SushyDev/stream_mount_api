408e510536962da0a4d809c7f6b83ad664eee418
xSushy <xsushy@hotmail.com>
2025-07-05 20:48:39 +0200

    custom error message support

--- api_errors.go
+++ api_errors.go
@@ -9,17 +9,20 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func ToResponseError(err error) error {
+// ToResponseError converts a filesystem error to a gRPC status error.
+// fsErrorType is the type of filesystem error to check against and will be used as the error code/type.
+// err is any error you wish to pass and will be used as the error message.
+func ToResponseError(fsErrorType error, err error) error {
 	if err == nil {
-		return nil
+	 		return status.Error(codes.Unknown, "error cannot be nil")
 	}
 
 	switch {
-	case os.IsNotExist(err):
+	case os.IsNotExist(fsErrorType):
 		return status.Error(codes.NotFound, err.Error())
-	case os.IsPermission(err):
+	case os.IsPermission(fsErrorType):
 		return status.Error(codes.PermissionDenied, err.Error())
-	case errors.Is(err, syscall.ENOENT):
+	case errors.Is(fsErrorType, syscall.ENOENT):
 		return status.Error(codes.NotFound, syscall.ENOENT.Error())
 	default:
 		return status.Error(codes.Unknown, err.Error())
